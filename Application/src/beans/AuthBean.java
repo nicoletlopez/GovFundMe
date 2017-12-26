fd94505a7f0403ab2b41222914cd1f4ec85939ff
Justin A. Parado <japarado@student.apc.edu.ph>
2017-12-26 23:52:51 +0800

    implemented login in frontend

--- Application/src/beans/AuthBean.java
+++ Application/src/beans/AuthBean.java
@@ -1,5 +1,29 @@
 package beans;
 
-public class AuthBean
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class AuthBean implements Serializable
 {
+    private String loggedUsername;
+
+    public String getLoggedUsername()
+    {
+        return loggedUsername;
+    }
+
+    public void setLoggedUsername(String loggedUsername)
+    {
+        this.loggedUsername = loggedUsername;
+    }
+
+    public String logout()
+    {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "index?faces-redirect=true";
+    }
 }
--- Application/src/beans/LoginBean.java
+++ Application/src/beans/LoginBean.java
@@ -1,10 +1,61 @@
 package beans;
 
+import models.daos.LoginDao;
+import models.services.LoginService;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean
 public class LoginBean
 {
     private String username;
     private String password;
+
+    @ManagedProperty(value = "#{authBean}")
+    private AuthBean authBean;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public AuthBean getAuthBean()
+    {
+        return authBean;
+    }
+
+    public void setAuthBean(AuthBean authBean)
+    {
+        this.authBean = authBean;
+    }
+
+    private LoginService loginService = new LoginDao();
+
+    public String login()
+    {
+        if (loginService.login(this.username, this.password))
+        {
+            authBean.setLoggedUsername(this.username);
+            return "projects";
+        } else
+        {
+            return "index";
+        }
+    }
 }
